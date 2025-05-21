b4f87b76a6a5d5bb32167189751425efc76d905f
Jeremiah Coyle <jcoyle@gmail.com>
2025-05-21 12:26:51 -0400

    Add fireworks dep

--- project.clj
+++ project.clj
@@ -2,8 +2,13 @@
   :description "Rich text console printing for Clojure(Script)"
   :url "https://github.com/paintparty/bling"
   :license {:name "MIT"}
+  :source-paths ["src"
+                 ;; for local dev of fireworks and lasertag deps
+                 ;; "../lasertag/src"
+                 "../fireworks/src"
+                 ]
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [io.github.paintparty/fireworks "0.11.0"]]
+                 [io.github.paintparty/fireworks "0.12.0-SNAPSHOT"]]
   :repl-options {:init-ns bling.core}
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
                                     :sign-releases false}]])
