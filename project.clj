3d197282977661824815f6e46e8b687a928cb983
Jeremiah Coyle <jcoyle@gmail.com>
2025-05-31 14:26:12 -0400

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.paintparty/bling "0.7.0-SNAPSHOT"
+(defproject io.github.paintparty/bling "0.7.0"
   :description "Rich text console printing for Clojure(Script)"
   :url "https://github.com/paintparty/bling"
   :license {:name "MIT"}
@@ -8,7 +8,7 @@
                  "../fireworks/src"
                  ]
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [io.github.paintparty/fireworks "0.12.0-SNAPSHOT"]
+                 [io.github.paintparty/fireworks "0.12.0"]
                  [metosin/malli "0.18.0"]]
   :repl-options {:init-ns bling.core}
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
